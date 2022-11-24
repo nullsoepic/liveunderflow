7b367a3c979d8dd0c444453baa3b2340ffd9c841
Null <83729285+nullsoepic@users.noreply.github.com>
2022-11-24 20:39:39 +0100

    Fix that actually works this time

--- Commands/general/vouch.ts
+++ Commands/general/vouch.ts
@@ -10,7 +10,8 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction, client: DrippyClient) {
     const embed = new EmbedBuilder()
     
-    if(!interaction.member.roles.cache.has(r => r.id === client.config.guild.roles.rw)) {
+    //@ts-ignore
+    if(!(interaction.member.roles as GuildMemberRoleManager).cache.has(r => r.id === client.config.guild.roles.rw)) {
         embed.setTitle(`You can't vouch.`);
         embed.setDescription(`Only users who have the \`rw\` role can vouch for others!`);
         embed.setColor(`Red`);
