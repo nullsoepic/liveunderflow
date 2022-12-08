fbc94106e08ada7ba43e001e1a5de6279f788ce6
ATXLtheAxolotl <flashtube107@gmail.com>
2022-12-07 20:19:29 -0500

    fix(): Fixed undefined message when you say a blacklisted phrase.

--- Events/Messages/MessageCreate.ts
+++ Events/Messages/MessageCreate.ts
@@ -1,11 +1,11 @@
+import { Message } from "discord.js"
 import { DiscordClient } from "../../Utils/DiscordClient"
 
 export const name = "messageCreate"
-export function execute(message, client: DiscordClient) {
-	if(blacklist(message.content)) {
-		message.delete()
-		message.channel.send({content: `:x: Your message contained a blacklisted phase ${message.member.tag}`})
-	}
+export function execute(message: Message, client: DiscordClient) {
+	if(!blacklist(message.content)) return;
+	message.delete()
+	message.channel.send({content: `:x: Your message contained a blacklisted phase <@!${message?.author.id}>!`})
 
 	function blacklist(string: string) {
 		return new RegExp(`(?:${client.config.filter.join("|")})`, "gi").test(string) ? true : false
