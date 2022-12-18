263179cbc9208a947df8d5a02bc4fc92dc4d5db6
ATXLtheAxolotl <flashtube107@gmail.com>
2022-12-18 16:33:30 -0500

    fix(): Remove unecessary `?`

--- Events/Messages/MessageCreate.ts
+++ Events/Messages/MessageCreate.ts
@@ -5,7 +5,7 @@ export const name = "messageCreate"
 export function execute(message: Message, client: DiscordClient) {
 	if(!blacklist(message.content)) return;
 	message.delete()
-	message.channel.send({content: `:x: Your message contained a blacklisted phase <@!${message?.author.id}>!`})
+	message.channel.send({content: `:x: Your message contained a blacklisted phase <@!${message.author.id}>!`})
 
 	function blacklist(string: string) {
 		return new RegExp(`(?:${client.config.filter.join("|")})`, "gi").test(string) ? true : false
